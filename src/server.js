061bccec9bdc468e604c275c26881b189e081db1
kc-clintone <kayseeclintone@gmail.com>
2025-11-06 14:32:26 +0300

    establishing a db connection

--- src/server.js
+++ src/server.js
@@ -1,5 +1,17 @@
+import sequelize from "./config/db.js";
+
 const PORT = process.env.PORT || 5000;
 
+const startServer = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Database connected successfully!");
 
-console.log("something is cooking")
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  } catch (error) {
+    console.error("Failed to start server:", error);
+  }
+};
+
+startServer();
 
