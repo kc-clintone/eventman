1df719cc481e3b6f6a1639d959896ebe4832ba9a
kc-clintone <kayseeclintone@gmail.com>
2025-11-07 10:53:07 +0300

    fixing cors issue with db conn --> changed db co fighration

--- src/config/db.js
+++ src/config/db.js
@@ -2,8 +2,13 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize({
   dialect: "postgres",
+  host: "db.zgstfiwkebxglynulwsm.supabase.co",
+  username: "postgres",
+  password: "bkhqC7fe7?&3LMu",
+  database: "postgres",
+  port: 5432,
   logging: false,
 });
 
