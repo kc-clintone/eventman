2eddeed0e97ab66122d2c543b78341c97695334d
kc-clintone <kayseeclintone@gmail.com>
2025-11-07 09:00:21 +0300

    added the events model --> untested

---
+++ src/models/events.model.js
@@ -0,0 +1,18 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+import User from "./user.model.js";
+
+const Event = sequelize.define("Event", {
+  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  title: { type: DataTypes.STRING, allowNull: false },
+  description: { type: DataTypes.TEXT },
+  date: { type: DataTypes.DATE, allowNull: false },
+  location: { type: DataTypes.STRING, allowNull: false },
+  maxAttendees: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 100 },
+});
+
+User.hasMany(Event, { foreignKey: "organizerId" });
+Event.belongsTo(User, { as: "organizer" });
+
+export default Event;
+
