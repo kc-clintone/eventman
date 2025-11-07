32fd211a1920c5fdc39511b2c18e051d7eb4f834
kc-clintone <kayseeclintone@gmail.com>
2025-11-07 19:16:38 +0300

    fixing typo in rsvp.models.js

--- src/models/rsvp.model.js
+++ src/models/rsvp.model.js
@@ -1,7 +1,7 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/db.js";
 import User from "./user.model.js";
-import Event from "./event.model.js";
+import Event from "./events.model.js";
 
 const RSVP = sequelize.define("RSVP", {
   status: { type: DataTypes.ENUM("pending", "approved"), defaultValue: "pending" },
