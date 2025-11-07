932cf2aca9ab72c53132d5b3ec046f8a916849d5
kc-clintone <kayseeclintone@gmail.com>
2025-11-07 15:13:57 +0300

    tiny bit changes -- fixed typos in the authController file

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+import User from "../models/user.model.js";
 
 const generateToken = (id, role) => {
   return jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: "7d" });
