5107cbc7415aad3e0f07ce910e234ef293fd238d
kc-clintone <kayseeclintone@gmail.com>
2025-11-07 15:22:54 +0300

    working on events route -- finishing protected routes logic

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -1,4 +1,13 @@
 import express from "express";
+import {
+  createEvent,
+  getEvents,
+  getEventById,
+  updateEvent,
+  deleteEvent
+} from "../controllers/eventController.js";
+
+import { protect, organizerOnly } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
