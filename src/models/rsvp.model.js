560b1334f3681236cc026f5f44d0e28df34602e4
kc-clintone <kayseeclintone@gmail.com>
2025-11-10 09:24:40 +0300

    fixed a bug ==> cannot get [User], user is not associated with [Event]

--- src/models/rsvp.model.js
+++ src/models/rsvp.model.js
@@ -7,8 +7,15 @@ const RSVP = sequelize.define("RSVP", {
   status: { type: DataTypes.ENUM("pending", "approved"), defaultValue: "pending" },
 });
 
+// Associating User to Event
 User.belongsToMany(Event, { through: RSVP, foreignKey: "userId" });
 Event.belongsToMany(User, { through: RSVP, foreignKey: "eventId" });
+
+// linking rsvp back to user (i had no quik fix)
+RSVP.belongsTo(User, { foreignKey: "userId" });
+RSVP.belongsTo(Event, { foreignKey: "eventId" });
+User.hasMany(RSVP, { foreignKey: "userId" });
+Event.hasMany(RSVP, { foreignKey: "eventId" });
 
 export default RSVP;
 
