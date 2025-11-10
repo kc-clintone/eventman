1a096326585b7e7d4fc5e802a490793c0325002b
kc-clintone <kayseeclintone@gmail.com>
2025-11-10 09:42:29 +0300

    altered user id to use unique id instead of plain integers -- for security

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,8 +1,13 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/db.js";
+import {nanoid} from 'nanoid';
 
 const User = sequelize.define("User", {
-  id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: () => nanoid(8),
+    primaryKey: true,
+  },
   name: { type: DataTypes.STRING, allowNull: false },
   email: { type: DataTypes.STRING, unique: true, allowNull: false },
   password: { type: DataTypes.STRING, allowNull: false },
