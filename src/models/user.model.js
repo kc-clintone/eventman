d0f804acf0fc4f13dde65dd403989485208ffe6a
kc-clintone <kayseeclintone@gmail.com>
2025-11-10 12:11:37 +0300

    fixing a bug -- changing user role from 'user' to 'attendee'

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -11,7 +11,7 @@ const User = sequelize.define("User", {
   name: { type: DataTypes.STRING, allowNull: false },
   email: { type: DataTypes.STRING, unique: true, allowNull: false },
   password: { type: DataTypes.STRING, allowNull: false },
-  role: { type: DataTypes.ENUM("user", "organizer"), defaultValue: "user" },
+  role: { type: DataTypes.ENUM("attendee", "organizer"), defaultValue: "attendee" },
 });
 
 export default User;
